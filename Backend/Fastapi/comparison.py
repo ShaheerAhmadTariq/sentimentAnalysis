cf0956fc9d1562773528d1d0d34e16c62ecde8a9
unknown <shaheerahmadtariq@gmail.com>
2023-01-11 18:37:35 +0500

    working line for comaprison page

--- Backend/Fastapi/comparison.py
+++ Backend/Fastapi/comparison.py
@@ -1,9 +1,72 @@
 from database import session,session1,session2,session3, SessionLocal
 from model import newsBrands, newsCompetitor, newsHashtag, redditBrands, redditCompetitor, redditHashtag, Base, projectSentiments
 from datetime import datetime, timedelta
-from sqlalchemy import select, func
+from sqlalchemy import select, func, Integer
 from collections import defaultdict
 from typing import List, Dict, Any
+
+
+def joinDict(first: List[Dict[str, Any]], second: List[Dict[str, Any]]):
+    for d in second:
+        for key, value in d.items():
+            found = False
+            for dict_ in first:
+                if key in dict_:
+                    dict_[key] += value
+                    found = True
+                    break
+            if not found:
+                first.append({key: value})
+    return first
+
+
+
+def comparisonLineChart(brand: str, competitor: str, hashtag: str, day: int ):
+    now = datetime.now()
+    days = now - timedelta(days=day)
+    n_brands = getNewsGraph(brand, days, newsBrands)
+    n_competitors = getNewsGraph(competitor, days, newsCompetitor)
+    n_hashtag = getNewsGraph(hashtag, days, newsHashtag)
+
+    r_brands = getNewsGraph(brand, days, redditBrands)
+    r_competitors = getNewsGraph(competitor, days, redditCompetitor)
+    r_hashtags = getNewsGraph(hashtag, days, redditHashtag)
+
+    result = joinDict(n_brands,n_competitors)
+    result = joinDict(result, n_hashtag)
+    result = joinDict(result, r_brands)
+    result = joinDict(result, r_competitors)
+    result = joinDict(result, r_hashtags)
+
+    return result
+
+def getNewsGraph(name: str, one_month_ago : int, table: str): 
+    query = (
+        session3.query(newsBrands.published_at, func.count(newsBrands.id).label("count").cast(Integer))
+        .filter(newsBrands.published_at >= one_month_ago)
+        .group_by(newsBrands.published_at)
+        .all()
+    )
+    result_as_dict = [{row[0].strftime("%Y-%m-%d"):row[1]} for row in query]
+    return result_as_dict
+
+# def getNewsGraph(name: str, one_month_ago : int, table: str): 
+#     query = (
+#         session.query(table.published_at, func.count(table.id))
+#         .filter(table.published_at >= one_month_ago)
+#         .group_by(table.published_at)
+#         .all()
+#     )
+#     result_as_dict = [{"published_at": row[0].strftime("%Y-%m-%d"), "count": row[1]} for row in query]
+#     return result_as_dict
+    # rows = session.query(table.content, func.date(table.published_at).label('published_at')).filter(table.published_at >= one_month_ago, table.name == name).all()
+    
+
+    # content_dict = defaultdict(list)
+    # for row in rows:
+    #     content_dict[row.published_at].append(row.content)
+
+    # return sentiment_dict
 
 def comparisonCountpie(brand: str, competitor: str, hashtag: str, day: int ):
     newsCount =  getCount(newsBrands, brand)
